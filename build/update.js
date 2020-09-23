1ecf14217f9f2ead722c284bf7a9003e17e7c3bb
Arturas <a.molcanovas@gmail.com>
2020-09-23 23:37:27 +0100

    ci: Refactor bumper script

--- build/update.js
+++ build/update.js
@@ -1,40 +1,93 @@
 /* eslint-disable no-magic-numbers */
 
-const [range, pkg] = process.argv.slice(2);
+let origVersion, pkgJsonPath, pkgJson = null;
+(async () => {
+  const [range, pkg] = process.argv.slice(2);
 
-if (!['major', 'minor', 'patch'].includes(range)) {
-  console.error('Invalid range:', range);
-  process.exit(1);
-}
+  if (!['major', 'minor', 'patch'].includes(range)) {
+    throw new Error(`Invalid range: ${range}`);
+  }
 
-const {join} = require('path');
-const fs = require('fs');
+  const {join, relative} = require('path');
+  const fs = require('fs');
 
-const pkgDir = join(__dirname, '..', 'packages', pkg);
-if (!fs.existsSync(pkgDir) || !fs.statSync(pkgDir).isDirectory()) {
-  console.error('Invalid package:', pkg);
-  process.exit(1);
-}
+  const packagesDir = join(__dirname, '..', 'packages');
+  const pkgDir = join(packagesDir, pkg);
+  if (!fs.existsSync(pkgDir) || !fs.statSync(pkgDir).isDirectory()) {
+    throw new Error(`Invalid package: ${pkg}`);
+  }
 
-const {inc, valid: isValidVersion} = require('semver');
-const pkgJsonPath = join(pkgDir, 'package.json');
-const pkgJson = require(pkgJsonPath);
-if (!pkgJson.version || !isValidVersion(pkgJson.version)) {
-  console.error('Invalid old package version:', pkgJson.version);
-}
-pkgJson.version = inc(pkgJson.version, range);
-fs.writeFileSync(pkgJsonPath, `${JSON.stringify(pkgJson, null, 2)}\n`);
+  const {inc, valid: isValidVersion} = require('semver');
+  pkgJsonPath = join(pkgDir, 'package.json');
+  pkgJson = require(pkgJsonPath);
+  origVersion = pkgJson.version;
+  if (!origVersion || !isValidVersion(origVersion)) {
+    throw new Error(`Invalid old package version: ${origVersion}`);
+  }
 
-const {spawnSync} = require('child_process');
+  pkgJson.version = inc(origVersion, range);
+  fs.writeFileSync(pkgJsonPath, `${JSON.stringify(pkgJson, null, 2)}\n`);
 
-const {status, error} = spawnSync('npm', ['publish'], {
-  cwd: pkgDir,
-  encoding: 'utf8',
-  env: process.env,
-  stdio: 'inherit'
-});
+  try {
+    fs.rmdirSync(join(__dirname, '..', 'dist'));
+  } catch {
+    // ach well..
+  }
 
-if (status || error) {
-  console.error(error || `publish exited with ${status}`);
-  process.exit(1);
-}
+  const {rollup} = require('rollup');
+  const configs = require('../rollup.config')({
+    dts: true,
+    fcjs2015: true,
+    fesm2015: true,
+    fesm5: true,
+    pkgs: pkg
+  });
+
+  const builds$ = configs
+    .map(async rollupConfig => {
+      let label = rollupConfig.input;
+      try {
+        label = relative(packagesDir, rollupConfig.input);
+        console.log('Building', label);
+        const build = await rollup(rollupConfig);
+        const outputs = rollupConfig.output
+          .map(outputConfig => build.write(outputConfig));
+
+        await Promise.all(outputs);
+        console.log('Built   ', label);
+      } catch (e) {
+        console.error('Failed building', label, e);
+        throw e;
+      }
+    });
+
+  await Promise.all(builds$);
+
+  const {spawnSync} = require('child_process');
+  const {status, error} = spawnSync('npm', ['publish'], {
+    cwd: join(__dirname, '..', 'dist', pkg),
+    encoding: 'utf8',
+    env: process.env,
+    stdio: 'inherit'
+  });
+
+  if (error instanceof Error) {
+    throw error;
+  }
+
+  if (status) {
+    throw new Error(`publish exited with ${status}`);
+  }
+})()
+  .catch(e => {
+    console.error(e);
+    if (origVersion) {
+      pkgJson.version = origVersion;
+      try {
+        require('fs').writeFileSync(pkgJsonPath, `${JSON.stringify(pkgJson, null, 2)}\n`);
+      } catch (ee) {
+        console.error('Failed ot restore package.json', ee);
+      }
+    }
+    process.exit(1);
+  });
