7f87628642205f8ef789854510a12a2248156d69
Arturas <a.molcanovas@gmail.com>
2020-10-10 20:11:53 +0100

    feat(array-types): Add ArrayElemType type

--- packages/array-types/index.ts
+++ packages/array-types/index.ts
@@ -3,3 +3,5 @@ export type Arrayish<T> = T[] | readonly T[];
 export type NonEmptyArray<T> = [T, ...T[]];
 
 export type OneOrMore<T> = T | Arrayish<T>;
+
+export type ArrayElemType<T extends Arrayish<any>> = T[0];
